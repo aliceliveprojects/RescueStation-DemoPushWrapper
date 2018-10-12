cd85151305e1d3a2df75aa993c24dab7b5b0bf28
Dave Mee <davemee@gmail.com>
2018-10-12 14:32:56 +0100

    basic communications server/app work

--- service/MessageService.js
+++ service/MessageService.js
@@ -60,7 +60,7 @@ exports.messagesPOST = function(body) {
       fullPayload.notification = NOTIFICATIONS[ data.message_type ];
     }
     delete data.message;
-    fullPayload.data = { "d": JSON.stringify(data) };
+    fullPayload.data = { "payload": JSON.stringify(data) };
     // set authorisation in headers
     var headers = {
       'Content-Type':'application/json',
